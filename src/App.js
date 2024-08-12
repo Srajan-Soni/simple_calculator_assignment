0832f519a40ed3e33d3f9785371794903eaf4cd2
Srajan-Soni <60183046+Srajan-Soni@users.noreply.github.com>
2024-08-12 10:56:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -69,34 +69,36 @@ function App() {
   }
 
   return (
-    <div className="App min-h-screen bg-gray-100 flex flex-col justify-center items-center">
-      <h1 className='text-blue-500 text-2xl font-bold  mb-10'>Simple Calculator</h1>
-      
-      <div className="mt-4 md:mt-5 w-full md:w-1/2 flex flex-col justify-center items-center">
-        <h1 className='font-bold text-lg text-center mb-4'>Add the String Values</h1>
-        <div className='flex flex-col gap-4 w-full shadow-lg p-6 md:p-10 bg-white rounded-lg'>
-       
-          <Input
-            placeholder="Enter Values"
-            className='border w-full md:w-3/4 lg:w-1/2 m-auto border-blue-300 rounded-md my-2'
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <Button className='bg-blue-500 w-1/2 md:w-1/4 m-auto hover:bg-blue-600 rounded-md' onClick={calculate}>
-            <p className='font-bold text-white'>Calculate</p>
-          </Button>
-          {result !== null && (
-            <div className='mt-4'>
-              <h2 className='text-lg md:text-xl font-bold text-center'>Result: {result}</h2>
-            </div>
-          )}
-          {error && (
-            <div className='mt-4 text-red-600'>
-              <h2 className='text-md md:text-lg font-bold text-center'>Error: {error}</h2>
-            </div>
-          )}
+    <div className="App min-h-screen bg-gray-100 flex flex-col">
+  
+        <h1 className="text-blue-500 text-2xl mt-5 font-bold text-center">Simple Calculator</h1>
+
+      <main className=" flex flex-col  items-center mt-20">
+        <div className="w-full md:w-1/3 flex flex-col justify-center items-center">
+          <h2 className="font-bold text-lg text-center mb-4">Add the String Values</h2>
+          <div className="flex flex-col gap-4 w-full shadow-lg p-6 md:p-10 bg-white rounded-lg">
+            <Input
+              placeholder="Enter Values"
+              className="border w-full md:w-3/4 lg:w-1/2 m-auto border-blue-300 rounded-md my-2"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <Button className="bg-blue-500 w-1/2 md:w-1/4 m-auto hover:bg-blue-600 rounded-md" onClick={calculate}>
+              <p className="font-bold text-white">Calculate</p>
+            </Button>
+            {result !== null && (
+              <div className="mt-4">
+                <h2 className="text-lg md:text-xl font-bold text-center">Result: {result}</h2>
+              </div>
+            )}
+            {error && (
+              <div className="mt-4 text-red-600">
+                <h2 className="text-md md:text-lg font-bold text-center">Error: {error}</h2>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
